deecb8dd36f1626e8e4f579ae572cfbea1fa79ba
ibmbt <ibrahimtahirbutt10@gmail.com>
2026-07-28 23:43:39 +0500

    blis matmul

--- inc/kernals/matmul.h
+++ inc/kernals/matmul.h
@@ -7,10 +7,14 @@
 #include "../backend/vk_ops.h"
 
 void kernel_matmul_cpu_f32_forward(
+    const float* a, const float* b, float* out,
+    const uint32_t M, const uint32_t N, const uint32_t K
 );
 
 void kernel_matmul_vulkan_f32_forward(
-    VkContext* ctx
+    VkContext* ctx,
+    const float* a, const float* b, float* out,
+    const uint32_t M, const uint32_t N, const uint32_t K
 );
 
 #endif
--- src/kernals/matmul.c
+++ src/kernals/matmul.c
@@ -1,3 +1,24 @@
-#include"../../inc/kernals/matmul.h"
-#include <stddef.h>
-#include <string.h>
+#include "../../inc/kernals/matmul.h"
+#include <blis/blis.h>
+
+void kernel_matmul_cpu_f32_forward(
+    const float* a, const float* b, float* out,
+    const uint32_t M, const uint32_t N, const uint32_t K
+) {
+    float alpha = 1.0f;
+    float beta = 0.0f;
+
+    dim_t rs_a = K, cs_a = 1;
+    dim_t rs_b = N, cs_b = 1;
+    dim_t rs_c = N, cs_c = 1;
+
+    bli_sgemm(
+        BLIS_NO_TRANSPOSE, BLIS_NO_TRANSPOSE,
+        M, N, K,
+        &alpha,
+        (float*)a, rs_a, cs_a,
+        (float*)b, rs_b, cs_b,
+        &beta,
+        out, rs_c, cs_c
+    );
+}
